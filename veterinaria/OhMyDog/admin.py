e93a2e9fed87855ad702db3ed2c8e51d4b281729
Juan Cruz Cassera Botta <juancasserab@gmail.com>
2023-05-08 19:31:42 -0300

    Tabla clientes creada

--- veterinaria/OhMyDog/admin.py
+++ veterinaria/OhMyDog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from OhMyDog.modelos.clientes.clientes import Cliente
 
 # Register your models here.
+
+admin.site.register(Cliente)
--- veterinaria/OhMyDog/modelos/clientes/__init__.py
+++ veterinaria/OhMyDog/modelos/clientes/__init__.py
@@ -1,6 +1,26 @@
-from OhMyDog.modelos import Clientes
+from OhMyDog.modelos.clientes.clientes import Cliente
 
-def agregar_cliente(datos):
-    cliente = Clientes(**datos)
+
+def agregar_cliente(nombre, email, telefono, contraseña):
+    cliente = Cliente(
+        nombre=nombre, email=email, telefono=telefono, contraseña=contraseña
+    )
     cliente.save()
+    print()
     return cliente
+
+
+def borrar_cliente():
+    pass
+
+
+def modificar_cliente():
+    pass
+
+
+def buscar_cliente_por():
+    pass
+
+
+def listar_clientes():
+    return Cliente.objects.all()
