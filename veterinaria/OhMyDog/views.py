0f7208fe947232148cabd912c9e0a497d59f1b15
Juan Cruz Cassera Botta <juancasserab@gmail.com>
2023-05-16 17:05:21 -0300

    Fixes en login y registrar

--- veterinaria/OhMyDog/views.py
+++ veterinaria/OhMyDog/views.py
@@ -16,67 +16,76 @@ def home(request):
 
 
 def register(request):
-    if request.method == 'POST':
-        email = request.POST['email']
-        cliente =  buscar_cliente_por_mail(email)
-        if cliente is None:            
-            nombre = request.POST['nombre']
-            telefono = request.POST['telefono']    
-            contraseña = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k=8))
-            cliente = agregar_cliente(nombre, email, telefono, )
+    if request.method == "POST":
+        email = request.POST["email"]
+        cliente = buscar_cliente_por_mail(email)
+        if cliente is None:
+            nombre = request.POST["nombre"]
+            telefono = request.POST["telefono"]
+            contraseña = "".join(
+                random.choices(
+                    string.ascii_uppercase + string.ascii_lowercase + string.digits, k=8
+                )
+            )
+            cliente = agregar_cliente(
+                nombre,
+                email,
+                telefono,
+            )
             Usuario.objects.create_user(email, contraseña, cliente)
             send_mail(
-                   "Constraseña de su cuenta de OhMyDog",
-                   f"La contraseña autogenerada es: {contraseña}",
-                   settings.EMAIL_HOST_USER,
-                   ["nahucapara@gmail.com"],
-                   fail_silently=False,
-                    )
-            messages.success(request, 'Tu cuenta ha sido creada exitosamente. Revisa tu correo para obtener tu contraseña.')
+                "Constraseña de su cuenta de OhMyDog",
+                f"La contraseña autogenerada es: {contraseña}",
+                settings.EMAIL_HOST_USER,
+                [email],
+                fail_silently=False,
+            )
+            messages.success(
+                request,
+                "Tu cuenta ha sido creada exitosamente. Revisa tu correo para obtener tu contraseña.",
+            )
         else:
-            messages.error(request, f'Cliente {email} ya existente.')
-        
-        return redirect('login')
-            
+            messages.error(request, f"Cliente {email} ya existente.")
+
+        return redirect("login")
+
     else:
         return render(request, "register.html")
-    
+
+
 def login_usuario(request):
-    if request.method == 'POST':
-        mail = request.POST['email']
-        password = request.POST['contraseña']
-        send_mail(
-                   "Constraseña de su cuenta de OhMyDog",
-                   f"La contraseña autogenerada es: {password} {mail}",
-                   settings.EMAIL_HOST_USER,
-                   ["nahucapara@gmail.com"],
-                   fail_silently=False,
-                    )
-        usuario = authenticate(request, mail=mail, password=password)    
-        if usuario is not None:
-            login(request, usuario)
-            if usuario.primer_inicio:
-                #Mostrar cambiar contraseña
-                alternar_primer_acceso(usuario.id)
-            
-            return redirect('home') # redirige a la página principal del sitio
+    if request.method == "POST":
+        mail = request.POST["email"]
+        password = request.POST["contraseña"]
+
+        usuario = authenticate(request, mail=mail, password=password)
+        if usuario is None:
+            messages.error(request, "Datos incorrectos o usuario no existente")
+            return redirect("login")  # redirecciona al login de nuevo
 
         else:
-            messages.error(request, 'Nombre de usuario o contraseña inválidos')
-    
-    return render(request, 'login.html')
+            login(request, usuario)
+            if usuario.primer_inicio:
+                # Llamar funcion para mostrar cambiar contraseña
+                alternar_primer_acceso(usuario.id)
+            else:
+                # Si no es el primer inicio se lo redirecciona al home
+                return redirect("home")
+
+    return render(request, "login.html")
 
 
-def logout_usuario (request):
+def logout_usuario(request):
     logout(request)
-    return redirect('home')
+    return redirect("home")
+
 
 def alternar_primer_acceso(id):
-    usuario = Usuario.objects.get(id = id)
+    usuario = Usuario.objects.get(id=id)
     usuario.primer_inicio = False
     usuario.save()
-    
 
-#def test_view(request):
+
+# def test_view(request):
 #    clientes = listar_clientes()
 #    return render(request, "test.html", {"clientes": clientes})
