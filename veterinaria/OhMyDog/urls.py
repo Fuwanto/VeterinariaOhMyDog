1e71c1e70ffa7ac48f7da724592e4cd716f90b8e
Antonella <fuwa08@hotmail.com>
2023-05-20 19:36:09 -0300

    Nuevo Home y preparativos para funcionalidades

--- veterinaria/OhMyDog/urls.py
+++ veterinaria/OhMyDog/urls.py
@@ -2,10 +2,17 @@ from django.urls import path
 
 from OhMyDog.views.home import home
 from OhMyDog.views.auth import register, login_usuario, logout_usuario
+from OhMyDog.views.clientes import mis_datos, mis_perros, mis_turnos, todos_los_clientes
+from OhMyDog.views.turnos import solicitudes_de_turnos
 
 urlpatterns = [
     path("", home, name="home"),
     path("register", register, name="register"),
     path("login", login_usuario, name="login"),
     path("logout", logout_usuario, name="logout"),
+    path("mis_datos", mis_datos, name="mis_datos"),
+    path("mis_perros", mis_perros, name="mis_perros"),
+    path("mis_turnos", mis_turnos, name="mis_turnos"),
+    path("clientes", todos_los_clientes, name="clientes"),
+    path("solicitudes_de_turnos", solicitudes_de_turnos, name="solicitudes_de_turnos")
 ]
