4b077afe8b83cfda2455836f04b54ff13a5d9688
Juan Cruz Cassera Botta <juancasserab@gmail.com>
2023-05-19 23:27:52 -0300

    Fix

--- veterinaria/OhMyDog/urls.py
+++ veterinaria/OhMyDog/urls.py
@@ -5,12 +5,6 @@ from OhMyDog.views.home import home
 from OhMyDog.views.auth import login_usuario, logout_usuario, register
 
 urlpatterns = [
-<<<<<<< HEAD
-    path("", views.home, name="home"),
-    path("register", views.register, name="register"),
-    path("login", views.login_usuario, name="login"),
-    path("logout", views.logout_usuario, name="logout")
-=======
     path("", home, name="home"),
     path("register", register, name="register"),
     path("login", login_usuario, name="login"),
