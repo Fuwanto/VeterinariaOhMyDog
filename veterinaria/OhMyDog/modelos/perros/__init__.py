b6ed6cb94052435c64f94bd1ab51404c2f4d721e
Juan Cruz Cassera Botta <juancasserab@gmail.com>
2023-05-31 16:05:27 -0300

    Deshabilitar perro y listar solo los perros HABILITADOS

--- veterinaria/OhMyDog/modelos/perros/__init__.py
+++ veterinaria/OhMyDog/modelos/perros/__init__.py
@@ -4,17 +4,32 @@ from OhMyDog.modelos.perros.perros import Perro
 
 
 def registrar_perro(dueño, nombre, raza, peso, descripcion, fecha_de_nacimiento, sexo):
-    perro = Perro(dueño=dueño, nombre=nombre, raza=raza, peso=peso, descripcion=descripcion, 
-                  fecha_de_nacimiento=fecha_de_nacimiento, sexo=sexo)
+    perro = Perro(
+        dueño=dueño,
+        nombre=nombre,
+        raza=raza,
+        peso=peso,
+        descripcion=descripcion,
+        fecha_de_nacimiento=fecha_de_nacimiento,
+        sexo=sexo,
+    )
     perro.save()
     return perro
 
 
 def buscar_perros_por_dueño(dueño):
-    return Perro.objects.filter(dueño=dueño)
+    return Perro.objects.filter(dueño=dueño, habilitado=True)
+
 
 def buscar_perro_por_nombre(nombre, cliente):
     try:
         return buscar_perros_por_dueño(cliente)
     except:
         return None
+
+
+def deshabilitar_perro(id):
+    perro = Perro.objects.get(id=id)
+    perro.habilitado = False
+    perro.save()
+    return perro
