b49cbf504d753958be7ae83b453592780d22e49d
Juan Cruz Cassera Botta <juancasserab@gmail.com>
2023-06-15 02:55:08 -0300

    Modelo búsquedas

--- veterinaria/OhMyDog/modelos/publicaciones/__init__.py
+++ veterinaria/OhMyDog/modelos/publicaciones/__init__.py
@@ -1,11 +1,17 @@
 from django.shortcuts import get_object_or_404
 from OhMyDog.modelos.publicaciones.adopciones import Adopcion
+from OhMyDog.modelos.publicaciones.busquedas import Busqueda
 from OhMyDog.modelos.tamaniosPerros.tamaniosPerros import TamanioPerro
 from OhMyDog.modelos.etapaVidaPerro.etapaVidaPerro import EtapaVidaPerro
 
+"""
+        ADOPCIONES
+"""
+
 
 def filtrar_adopciones_por_cliente(cliente):
-    return Adopcion.objects.filter(cliente = cliente)
+    return Adopcion.objects.filter(cliente=cliente)
+
 
 def buscar_adopcion_por_nombre_y_cliente(cliente, nombre):
     try:
@@ -13,9 +19,10 @@ def buscar_adopcion_por_nombre_y_cliente(cliente, nombre):
     except:
         return None
 
+
 def agregar_adopcion(cliente, nombre, descripcion, tamanio_perro_id, etapa_vida_perro_id, sexo, castrado):
-    tamanio_perro = TamanioPerro.objects.get(id = tamanio_perro_id)
-    etapa_vida_perro = EtapaVidaPerro.objects.get(id = etapa_vida_perro_id)
+    tamanio_perro = TamanioPerro.objects.get(id=tamanio_perro_id)
+    etapa_vida_perro = EtapaVidaPerro.objects.get(id=etapa_vida_perro_id)
     adopcion = Adopcion(
         cliente=cliente,
         nombre=nombre,
@@ -23,20 +30,65 @@ def agregar_adopcion(cliente, nombre, descripcion, tamanio_perro_id, etapa_vida_
         tamanio_perro=tamanio_perro,
         etapa_vida_perro=etapa_vida_perro,
         sexo=sexo,
-        castrado=castrado
+        castrado=castrado,
     )
     adopcion.save()
     return adopcion
 
+
 def listar_adopciones():
     return Adopcion.objects.all()
+
 
 def adoptar(adopcion_id):
     adopcion = get_object_or_404(Adopcion, id=adopcion_id)
     adopcion.adoptado = True
     adopcion.save()
-    
-    
+
+
 def eliminar_publicacion_adopcion(adopcion_id):
     adopcion = get_object_or_404(Adopcion, id=adopcion_id)
     adopcion.delete()
+
+
+"""
+        BUSQUEDAS
+"""
+
+
+def agregar_busqueda(cliente, nombre, descripcion, zona, foto):
+    busqueda = Busqueda(
+        cliente=cliente,
+        nombre=nombre,
+        descripcion=descripcion,
+        zona=zona,
+        foto=foto,
+    )
+    busqueda.save()
+    return busqueda
+
+
+def filtrar_busquedas_por_cliente(cliente):
+    return Busqueda.objects.filter(cliente=cliente)
+
+
+def listar_busquedas():
+    return Busqueda.objects.all()
+
+
+def eliminar_publicacion_busqueda(busqueda_id):
+    busqueda = get_object_or_404(Busqueda, id=busqueda_id)
+    busqueda.delete()
+
+
+def se_encontro(busqueda_id):
+    busqueda = get_object_or_404(Busqueda, id=busqueda_id)
+    busqueda.encontrado = True
+    busqueda.save()
+
+
+def buscar_busqueda_por_nombre_archivo_y_cliente(cliente, nombre_archivo):
+    try:
+        return Busqueda.objects.get(cliente=cliente, foto__contains=nombre_archivo)
+    except:
+        return None
---
+++ veterinaria/OhMyDog/modelos/publicaciones/busquedas.py
@@ -0,0 +1,27 @@
+from django.db import models
+from OhMyDog.modelos.clientes.clientes import Cliente
+from django.db import models
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
+import os
+
+
+class Busqueda(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    cliente = models.ForeignKey(Cliente, null=False, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=30)
+    descripcion = models.TextField(null=True)
+    zona = models.TextField()
+    encontrado = models.BooleanField(default=False)
+    foto = models.ImageField(upload_to="fotos/")
+
+    def __str__(self):
+        return f"Cliente: {self.cliente_id}, Nombre: {self.nombre}, Descripcion: {self.descripcion}, Zona: {self.zona}"
+
+
+@receiver(pre_delete, sender=Busqueda)
+def borrar_foto(sender, instance, **kwargs):
+    if instance.foto:
+        path_foto = instance.foto.path
+        if os.path.exists(path_foto):
+            os.remove(path_foto)
