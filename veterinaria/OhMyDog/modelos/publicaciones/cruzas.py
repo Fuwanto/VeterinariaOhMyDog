50e464542751fcccb623f3c839fa32c065d0b1ab
Juan Cruz Cassera Botta <juancasserab@gmail.com>
2023-07-10 15:31:20 -0300

    Se borra la foto al borrar una cruza

--- veterinaria/OhMyDog/modelos/publicaciones/cruzas.py
+++ veterinaria/OhMyDog/modelos/publicaciones/cruzas.py
@@ -1,5 +1,9 @@
 from django.db import models
 from OhMyDog.modelos.clientes.clientes import Cliente
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
+import os
+
 
 class Cruza(models.Model):
     id = models.BigAutoField(primary_key=True)
@@ -12,11 +16,18 @@ class Cruza(models.Model):
     peso = models.DecimalField(blank=False, max_digits=5, decimal_places=2)
     color = models.CharField(max_length=30)
     antecedentes_salud = models.TextField(null=True)
-    foto = foto = models.ImageField(upload_to="fotos/")
-    intereses = models.ManyToManyField('self', blank=True,  symmetrical=False)
-    ultimo_celo = models.DateField(blank=True, null=True) 
-    
+    foto = models.ImageField(upload_to="fotos/")
+    intereses = models.ManyToManyField("self", blank=True, symmetrical=False)
+    ultimo_celo = models.DateField(blank=True, null=True)
+
 
 def __str__(self):
     return f"Cliente: {self.cliente}, Nombre de perro: {self.nombre}"
-    
+
+
+@receiver(pre_delete, sender=Cruza)
+def borrar_foto(sender, instance, **kwargs):
+    if instance.foto:
+        path_foto = instance.foto.path
+        if os.path.exists(path_foto):
+            os.remove(path_foto)
